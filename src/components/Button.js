4a173d83bca092ec19b643032c4482f41143ed40
brighton0102 <mtandibrighton0102@gmail.com>
2023-09-21 23:24:22 +0200

    Add logic to the application

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,16 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
-const Button = ({ label, type }) => (
-  <button className={`btn-item ${type}`} type="button">
+const Button = ({ label, type, onClick }) => (
+  <button className={`btn-item ${type}`} type="button" onClick={onClick}>
     {label}
   </button>
 );
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired, // 'type' is optional with a default value
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
