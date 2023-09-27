200281ba03aad6dcdf1da8f470ae65e4ce417628
brighton0102 <mtandibrighton0102@gmail.com>
2023-09-27 22:47:04 +0200

    Create full website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
+// import { Route, Router, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import QuoteDisplay from './components/QuoteDisplay';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Calculator</h1>
-      <Calculator />
-      <QuoteDisplay />
-    </div>
+    <Router>
+      <nav>
+        <div className="title">Math Magicians</div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<QuoteDisplay />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,30 +16,33 @@ const Calculator = () => {
   };
 
   return (
-    <section className="container">
-      <div className="display">{calculatorState.next || calculatorState.total || '0'}</div>
-      <div className="btn-grid">
-        <Button label="AC" onClick={() => handleButtonClick('AC')} />
-        <Button label="+/-" onClick={() => handleButtonClick('+/-')} />
-        <Button label="%" onClick={() => handleButtonClick('%')} />
-        <Button label="÷" onClick={() => handleButtonClick('÷')} />
-        <Button label="7" onClick={() => handleButtonClick('7')} />
-        <Button label="8" onClick={() => handleButtonClick('8')} />
-        <Button label="9" onClick={() => handleButtonClick('9')} />
-        <Button label="x" onClick={() => handleButtonClick('x')} />
-        <Button label="4" onClick={() => handleButtonClick('4')} />
-        <Button label="5" onClick={() => handleButtonClick('5')} />
-        <Button label="6" onClick={() => handleButtonClick('6')} />
-        <Button label="-" onClick={() => handleButtonClick('-')} />
-        <Button label="1" onClick={() => handleButtonClick('1')} />
-        <Button label="2" onClick={() => handleButtonClick('2')} />
-        <Button label="3" onClick={() => handleButtonClick('3')} />
-        <Button label="+" onClick={() => handleButtonClick('+')} />
-        <Button label="0" type="double" onClick={() => handleButtonClick('0')} />
-        <Button label="." onClick={() => handleButtonClick('.')} />
-        <Button label="=" type="equal" onClick={() => handleButtonClick('=')} />
-      </div>
-    </section>
+    <div className="calculator-container">
+      <p className="math-description">Let&apos;s do some Math!</p>
+      <section className="container">
+        <div className="display">{calculatorState.next || calculatorState.total || '0'}</div>
+        <div className="btn-grid">
+          <Button label="AC" onClick={() => handleButtonClick('AC')} />
+          <Button label="+/-" onClick={() => handleButtonClick('+/-')} />
+          <Button label="%" onClick={() => handleButtonClick('%')} />
+          <Button label="÷" onClick={() => handleButtonClick('÷')} />
+          <Button label="7" onClick={() => handleButtonClick('7')} />
+          <Button label="8" onClick={() => handleButtonClick('8')} />
+          <Button label="9" onClick={() => handleButtonClick('9')} />
+          <Button label="x" onClick={() => handleButtonClick('x')} />
+          <Button label="4" onClick={() => handleButtonClick('4')} />
+          <Button label="5" onClick={() => handleButtonClick('5')} />
+          <Button label="6" onClick={() => handleButtonClick('6')} />
+          <Button label="-" onClick={() => handleButtonClick('-')} />
+          <Button label="1" onClick={() => handleButtonClick('1')} />
+          <Button label="2" onClick={() => handleButtonClick('2')} />
+          <Button label="3" onClick={() => handleButtonClick('3')} />
+          <Button label="+" onClick={() => handleButtonClick('+')} />
+          <Button label="0" type="double" onClick={() => handleButtonClick('0')} />
+          <Button label="." onClick={() => handleButtonClick('.')} />
+          <Button label="=" type="equal" onClick={() => handleButtonClick('=')} />
+        </div>
+      </section>
+    </div>
   );
 };
 
